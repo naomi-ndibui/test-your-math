ac462de0dc9e1d86e6b199eb8cb64fcfd12f1c04
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-05-02 08:23:55 +0300

    added public scanner object for use in all methods and imported java.util

--- src/main/java/Math.java
+++ src/main/java/Math.java
@@ -1,7 +1,9 @@
-
+import java.util.*;
 
 public class Math {
+       public static Scanner keyboard = new Scanner(System.in);
        public static void main(String [] args){
            System.out.print("How many math questions do you want: ");
+
        }
 }
