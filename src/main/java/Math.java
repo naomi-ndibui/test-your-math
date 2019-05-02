0779f7a53112e845d4e2629f0b2a35b50f566d92
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-05-02 08:35:45 +0300

    created an object of class scanner that has a nextInt function that checks if the input entered by the user is an interger, then declared an int variable to store the value

--- src/main/java/Math.java
+++ src/main/java/Math.java
@@ -2,8 +2,11 @@ import java.util.*;
 
 public class Math {
        public static Scanner keyboard = new Scanner(System.in);
-       public static void main(String [] args){
+       public static void main(String [] args)
+       {
            System.out.print("How many math questions do you want: ");
+           int j = keyboard.nextInt();
 
+           keyboard.nextInt();
        }
 }
